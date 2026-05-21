8629e675dd6678a22f2f9988ff5d0bf10c9fe53f
Riyan <riyanmayy@gmail.com>
2026-05-21 23:39:24 +0700

    first commit

---
+++ HHeloo-WWorld.py
@@ -0,0 +1,6 @@
+# Program Halo world
+print("================================")
+print("          Halo world")
+print("================================")
+print("Riyan Maysandi")
+print("Program Berhasil DIbuat")
